d79d2e4b633cd73b780a0fe613a4776a30832b64
Gorav Ved <gorav.ved@GALAXY.RADIXWEB.COM>
2023-07-10 14:22:11 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+
+
+
+// Header scrolling sticky Header
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 50) {
+    const headerHeightn = jQuery('#dynamic').outerHeight()
+     console.log(headerHeightn);
+      $('#dynamic').css("top", `${-headerHeightn}px`);
+     $('#nav-top').addClass('nav-top');     
+  } else {
+     $('#dynamic').css("top","0").removeClass('nav-top');
+    $('#nav-top').removeClass('nav-top');    
+  }
+});
+
+
+
+/* Search-bar */
+$(document).ready(function() {
+  $('.show-hidden-menu').click(function() {
+    $('.hidden-menu').slideToggle("slow");
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
